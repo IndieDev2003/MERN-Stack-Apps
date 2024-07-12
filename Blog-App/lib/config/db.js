14519b5d94dc84941678ad0ef31968f54ee74e71
Gagan Suman <ilovescience584@gmail.com>
2024-07-12 18:19:03 +0530

    Restart

--- Blog-App/lib/config/db.js
+++
@@ -1,8 +0,0 @@
-import moongoose from "moongoose";
-
-export const ConnectDB = async () => {
-  await moongoose.connect(
-    "mongodb+srv://fbdgagansuman:localvocal@blog-app-cluster.bijbc2l.mongodb.net/blog-app-cluster"
-  );
-  console.log("DB Connect");
-};
